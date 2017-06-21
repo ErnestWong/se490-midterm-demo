d317e3eef8ac528f328a8a760c6aefa7400c8d51
Parsa Alipour <parsaalipour@hotmail.com>
2017-06-20 20:37:14 -0400

    finish up UI

---
+++ js/SECE.js
@@ -0,0 +1,13 @@
+(function () {
+	document.getElementsByClassName('upload-file')[0].onclick = function(e){
+		document.getElementById('file-input').click();
+	}
+
+	document.getElementById('file-input').onchange = function() {
+		debugger;
+    	document.getElementsByClassName('progress-barr')[0].style.width = "800px";
+  		document.getElementsByClassName('download-file')[0].style.display = "inline-block";
+  		document.getElementsByClassName('upload-file')[0].style.display = "none";
+  	}
+
+}());
