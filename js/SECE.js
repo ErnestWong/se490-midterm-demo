8c3bb492d096d397611dc31e3078fb12836e9d2f
Parsa Alipour <parsaalipour@hotmail.com>
2017-07-17 16:48:30 -0400

    detect incorrect file extension for audio files

--- js/SECE.js
+++ js/SECE.js
@@ -4,9 +4,23 @@
 	}
 
 	document.getElementById('file-input').onchange = function() {
-    	document.getElementsByClassName('progress-barr')[0].style.width = "800px";
-  		document.getElementsByClassName('download-file')[0].style.display = "inline-block";
-  		document.getElementsByClassName('upload-file')[0].style.display = "none";
+  		var fileName = $(this).val().split('/').pop().split('\\').pop();
+  		var acceptedFileExtensions = ['.aiff', '.wav', '.mp3'];
+  		var fileHasValidExt = false;
+
+  		acceptedFileExtensions.forEach(function(acceptedFileExtension) {
+  			if (fileName.endsWith(acceptedFileExtension)) {
+  				fileHasValidExt = true;
+  			}
+  		});
+
+    	if (fileHasValidExt) {
+	    	document.getElementsByClassName('progress-barr')[0].style.width = "800px";
+	  		document.getElementsByClassName('download-file')[0].style.display = "inline-block";
+	  		document.getElementsByClassName('upload-file')[0].style.display = "none";
+    	} else {
+    		// invalid file is uploaded
+    	}
   	}
 
 }());
