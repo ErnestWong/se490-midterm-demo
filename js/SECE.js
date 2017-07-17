ed5d010b6c422e2f8852fbdd85799a23fcdd686c
Peter Li <tyuo9980@gmail.com>
2017-07-17 19:14:12 -0400

    split screen, nav

--- js/SECE.js
+++ js/SECE.js
@@ -20,7 +20,35 @@
 	  		document.getElementsByClassName('upload-file')[0].style.display = "none";
     	} else {
     		// invalid file is uploaded
+            $("#attenuate-err").html("File does not meet requirements");
     	}
   	}
 
+
+
 }());
+
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
