ea6ebcf2a97465e446f0d2eea798277a2e79fd4d
Peter Li <tyuo9980@gmail.com>
2017-07-19 05:03:00 -0400

    fuck yeah??

--- js/SECE.js
+++ js/SECE.js
@@ -1,12 +1,19 @@
 (function () {
-	document.getElementsByClassName('upload-file')[0].onclick = function(e){
-		document.getElementById('file-input').click();
-	}
+	$("#atn-upload-btn").click(function(){
+        $('#atn-file-input').click();
+    });
 
-	document.getElementById('file-input').onchange = function() {
+    $("#train-upload-btn").click(function(){
+        $('#train-file-input').click();
+    });
+
+	$('#atn-file-input').change(function(){
   		var fileName = $(this).val().split('/').pop().split('\\').pop();
+        var fileNameProc = "proc_" + fileName;
   		var acceptedFileExtensions = ['.aiff', '.wav', '.mp3'];
   		var fileHasValidExt = false;
+
+        $('#attenuate-bar').width("0px");
 
   		acceptedFileExtensions.forEach(function(acceptedFileExtension) {
   			if (fileName.endsWith(acceptedFileExtension)) {
@@ -15,40 +22,56 @@
   		});
 
     	if (fileHasValidExt) {
-	    	document.getElementsByClassName('progress-barr')[0].style.width = "800px";
-	  		document.getElementsByClassName('download-file')[0].style.display = "inline-block";
-	  		document.getElementsByClassName('upload-file')[0].style.display = "none";
+            $("#attenuate-bar").addClass("notransition");
+            $('#attenuate-bar').width("0");
+            $("#attenuate-bar").removeClass("notransition");
+	    	$('#attenuate-bar').width("100%");
+            setTransition("#attenuate-bar", "8s");
+
+            setTimeout(function() {
+                $("#clips").show();
+                //$("#download-file").css("display", "inline-block");
+
+                $('#original').attr("src", fileName);
+                $('#processed').attr("src", fileNameProc);
+            }, 8000);
     	} else {
     		// invalid file is uploaded
             $("#attenuate-err").html("File does not meet requirements");
     	}
-  	}
+  	});
 
+    $('#train-file-input').change(function(){
+        var fileName = $(this).val().split('/').pop().split('\\').pop();
+        var acceptedFileExtensions = ['.aiff', '.wav', '.mp3'];
+        var fileHasValidExt = false;
 
+        $('#train-bar').width("0px");
 
+        acceptedFileExtensions.forEach(function(acceptedFileExtension) {
+            if (fileName.endsWith(acceptedFileExtension)) {
+                fileHasValidExt = true;
+            }
+        });
+
+        if (fileHasValidExt) {
+            $("#train-bar").addClass("notransition");
+            $('#train-bar').width("0px");
+            $("#train-bar").removeClass("notransition");
+            setTransition("#train-bar", "5s");
+            $('#train-bar').width("100%");
+
+            setTimeout(function() {
+                $("#train-list").append($("<li>").text(fileName));
+            }, 5000);
+        } else {
+            // invalid file is uploaded
+            $("#train-err").html("File does not meet requirements");
+        }
+    });
+
+    function setTransition(e, dur){
+        $(e).css("-webkit-transition", "width " + dur);
+        $(e).css("transition", "width " + dur);
+    }
 }());
-
-function createCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name,"",-1);
-}
